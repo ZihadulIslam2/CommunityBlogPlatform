a7d1f1de398ae7c9b60bf8b6fb4f1787df4610bc
Zihadul Islam <zihadul708@gmail.com>
2024-08-30 09:27:05 +0400

    save and recive data from mongodb

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 // express app
 // invoking the express
 const app = express()
 
 // mongodb connection
+
 const dbURI =
   'mongodb+srv://zihadul708:01882343242@nodetuts.xnfrv.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts'
 
@@ -59,6 +61,35 @@ app.get('/about-me', (req, res) => {
   res.redirect('/about')
 })
 
+// getting and saving data to mongodb
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog',
+  })
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result) // Send the result back as a response
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).send('Error saving the blog') // Sending a response in case of error
+    })
+})
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'create blogs' })
 })
@@ -66,5 +97,5 @@ app.get('/blogs/create', (req, res) => {
 //404 page
 //use methods run every time.
 app.use((req, res) => {
-  res.status(404).render('404')
+  res.status(404).render('404', { title: 'page not found.' })
 })
